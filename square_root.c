df5fcb7ccc8b2c5c36b73d053a00f34098ed59f9
Nicole Makarowski <nmakarowski@gmail.com>
2022-07-03 17:09:22 -0700

    Cubed root fixed point arithmetic

--- square_root.c
+++ square_root.c
@@ -20,7 +20,7 @@ uint32_t fixed_multiplication(uint32_t x, uint32_t y, int n_bits){
   return result;
 }
 
-uint32_t square_root(uint32_t M, int K){ //Square root of (M) with K bits of precision
+uint32_t square_root(float M, int K){ //Square root of (M) with K bits of precision
   uint32_t f = 1 << K;
   uint32_t f_sqrt = 1 << K;
   uint32_t u, u_sqrt, temp;
@@ -34,34 +34,34 @@ uint32_t square_root(uint32_t M, int K){ //Square root of (M) with K bits of pre
       f_sqrt = u_sqrt;
     }
   }
-  return f_sqrt;
+  return fixed_to_float(f_sqrt, K);
 }
 
 
 
 float cubic_root(float M, int K){ //Cubic root of (M) with K bits of precision
-  float f = 1.0;
-  float f_sqrt_3 = 1.0;
+  uint32_t f =  1 << K;
+  uint32_t f_sqrt_3 =  1 << K;
   int i;
-  float u, u_sqrt_3, temp;
+  uint32_t u, u_sqrt_3, temp;
   for (i = 0; i < K-1; i++){
-    temp = 1.0 / (1 << i); //2 ^ -i
-    u = f * (1 + temp) * (1 + temp) * (1 + temp);
-    u_sqrt_3 = f_sqrt_3 * (1 + temp);
-    if (u < M){
+    temp =((1.0 / (1 << i)) + 1.0) * (1 << K); //2 ^ -i + 1
+    u =  fixed_multiplication(f, fixed_multiplication(temp, fixed_multiplication(temp, temp, K), K), K);
+    u_sqrt_3 = fixed_multiplication(f_sqrt_3, temp, K);
+    if (u < M * (1 << K)){
       f = u;
       f_sqrt_3 = u_sqrt_3;
     }
   }
-  return f_sqrt_3;
+  return fixed_to_float(f_sqrt_3, K);
 }
 
 int main(int argc, char *argv[]){
-  uint32_t n = 4;
+  float n = 4;
   double x = sqrt(n);
-  uint32_t y = square_root(n, 15);
+  float y = square_root(n, 15);
   
-  printf("Square Root of %f\nsqrt function: %f\nccm: %f\n", n, x, fixed_to_float(y, 15));
+  printf("Square Root of %f\nsqrt function: %f\nccm: %f\n", n, x, y);
 
   float n3 = 16;
   double x3 = cbrt(n);
